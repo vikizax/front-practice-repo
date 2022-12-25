8fd9a78760b8ee7a009729325f4b38f572df9b60
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-12-26 00:27:27 +0530

    updated deepCloneObject solution

--- 36/index.js
+++ 36/index.js
@@ -31,10 +31,12 @@ function deepCloneObject(target) {
 
   Object.keys(target).forEach((key) => {
     const value = target[key];
-    if (Array.isArray(value) || typeof value !== "object") {
-      result[key] = value;
-    } else {
+    if (Array.isArray(value)) {
+      result[key] = Array.from(value);
+    } else if (typeof value === "object") {
       result[key] = deepCloneObject(value);
+    } else {
+      result[key] = value;
     }
   });
   return result;
@@ -47,6 +49,7 @@ const testObject = {
     zipcode: 123123,
     country: "country",
   },
+  ids: [1, 2, 4, 9],
   contactDetails: {
     phone: {
       personal: 1231231231,
@@ -59,6 +62,9 @@ const testObject2 = testObject;
 const result = deepCloneObject(testObject);
 
 testObject.address.country = "india";
+testObject.ids.push(10);
 console.log({ test_country: testObject.address.country });
 console.log({ test2_country: testObject2.address.country });
-console.log({ result_country: result.address?.country });
+console.log({ test1_ids: testObject2.ids });
+console.log({ result_country: result?.address?.country });
+console.log({ result_ids: result?.ids });
